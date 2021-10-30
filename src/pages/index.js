00383da13bb51254724b5bde4be0c26da94ae7b5
G7M7T1 <genlisense@gmail.com>
2021-10-30 06:52:22 -0400

    update meta
    update meta and og meta and img

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import clsx from "clsx";
 import Layout from "@theme/Layout";
 import Link from "@docusaurus/Link";
+import Head from "@docusaurus/Head";
 import useDocusaurusContext from "@docusaurus/useDocusaurusContext";
 import styles from "./index.module.css";
 import HomepageFeatures from "../components/HomepageFeatures";
@@ -31,8 +32,39 @@ export default function Home() {
   return (
     <Layout
       title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />"
+      description="Notebook - Made by G7M7T1 and Xpakeb13 and AsheOne - Welcome to the website, here are our study notes and our blog"
     >
+      <Head>
+        <meta name="author" content="G7M7T1" />
+        <meta name="robots" content="index,follow" />
+        <meta property="og:site_name" content="Notebook" />
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="https://notebook-lang.vercel.app/" />
+        <meta property="og:title" content="Notebook" />
+        <meta
+          property="og:description"
+          content="Notebook - Made by G7M7T1 and Xpakeb13 and AsheOne - Welcome to the website, here are our study notes and our blog"
+        />
+        <meta
+          property="og:image"
+          content="https://notebook-lang.vercel.app/img/11.png"
+        />
+
+        <meta property="twitter:card" content="summary_large_image" />
+        <meta
+          property="twitter:url"
+          content="https://notebook-lang.vercel.app/"
+        />
+        <meta property="twitter:title" content="Notebook" />
+        <meta
+          property="twitter:description"
+          content="Notebook - Made by G7M7T1 and Xpakeb13 and AsheOne - Welcome to the website, here are our study notes and our blog"
+        />
+        <meta
+          property="twitter:image"
+          content="https://notebook-lang.vercel.app/img/11.png"
+        />
+      </Head>
       <HomepageHeader />
       <main>
         <HomepageFeatures />
