5f651a1e810efb921eba34a9b5b744d5459b7592
G7M7T1 <genlisense@gmail.com>
2021-10-31 12:15:47 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -116,7 +116,7 @@ const config = {
       prism: {
         theme: lightCodeTheme,
         darkTheme: darkCodeTheme,
-        additionalLanguages: ["java"],
+        additionalLanguages: ["java, sql, powershell"],
       },
     }),
 };
